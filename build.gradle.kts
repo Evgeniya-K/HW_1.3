c1f5c5de33caf09aa4f1f07d92e9f0c856bcaeef
Evgeniya-K <E-S-Kondrashina@yandex.ru>
2023-05-29 23:41:54 +0300

    добавила автотесты на функцию fee из прошлого дз

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
+
+
+
 plugins {
     kotlin("jvm") version "1.7.10"
+    jacoco
 }
 
 group = "ru.netology"
@@ -12,12 +17,13 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    testImplementation("junit:junit:4.13.2")
+
+
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
---
+++ src/test/kotlin/ru/netology/Hw_1_3_2KtTest.kt
@@ -0,0 +1,97 @@
+package ru.netology
+
+import fee
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class Hw_1_3_2KtTest {
+
+    @Test
+    fun fee_visaMinCommission() {
+        val paymentMethod = "Visa"
+        val valueTransfer = 100
+        val valueMonthTransfer = 50_000
+
+        val result = fee(paymentMethod, valueTransfer, valueMonthTransfer)
+
+        assertEquals(35,result)
+    }
+
+    @Test
+    fun fee_visaErrorValueMonthTransferCommission() {
+        val paymentMethod = "Visa"
+        val valueTransfer = 100
+        val valueMonthTransfer = 650_000
+
+        val result = fee(paymentMethod, valueTransfer, valueMonthTransfer)
+
+        assertEquals(-2,result)
+    }
+
+    @Test
+    fun fee_vkPayCommission() {
+        val paymentMethod = "VK Pay"
+        val valueTransfer = 1000
+        val valueMonthTransfer = 30_000
+
+        val result = fee(paymentMethod, valueTransfer, valueMonthTransfer)
+
+        assertEquals(0,result)
+    }
+
+    @Test
+    fun fee_vkPayErrorValueTransferCommission() {
+        val paymentMethod = "VK Pay"
+        val valueTransfer = 20_000
+        val valueMonthTransfer = 30_000
+
+        val result = fee(paymentMethod, valueTransfer, valueMonthTransfer)
+
+        assertEquals(-2,result)
+    }
+
+    @Test
+    fun fee_MaestroCommission() {
+        val paymentMethod = "Maestro"
+        val valueTransfer = 20_000
+        val valueMonthTransfer = 30_000
+
+        val result = fee(paymentMethod, valueTransfer, valueMonthTransfer)
+
+        assertEquals(0,result)
+    }
+
+    @Test
+    fun fee_MaestroAddCommission() {
+        val paymentMethod = "Maestro"
+        val valueTransfer = 20_000
+        val valueMonthTransfer = 60_000
+
+        val result = fee(paymentMethod, valueTransfer, valueMonthTransfer)
+
+        assertEquals(140,result)
+    }
+
+    @Test
+    fun fee_MaestroErrorValueTransferCommission() {
+        val paymentMethod = "Maestro"
+        val valueTransfer = 200_000
+        val valueMonthTransfer = 60_000
+
+        val result = fee(paymentMethod, valueTransfer, valueMonthTransfer)
+
+        assertEquals(-2,result)
+    }
+
+    @Test
+    fun fee_errorTypePaymentMethod() {
+        val paymentMethod = "apple pay"
+        val valueTransfer = 20_000
+        val valueMonthTransfer = 60_000
+
+        val result = fee(paymentMethod, valueTransfer, valueMonthTransfer)
+
+        assertEquals(-1,result)
+    }
+}
